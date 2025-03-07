3c9b399584070ad84072df22fa5fcd043e55ecad
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-03-07 23:54:22 +0500

    done with dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Herosection from "../components/Herosection";
+import FeaturedCourses from "../components/FeaturedCourses";
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
    <Herosection/>
+   <FeaturedCourses/>
+    
    </div>
   );
 }
--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -9,18 +9,44 @@ function Herosection() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
+
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-t from-neutral-50 to-neutral-400">
           Master the art of music
         </h1>
+
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you're a beginner or looking to refine your
+          journey today. Whether you are a beginner or looking to refine your
           skills, join us to unlock your true potential.
         </p>
+
         <div className="mt-4">
           <Link href={"/courses"}>
-            <button>Explore courses</button>
+            <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+              </span>
+              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <span>All Courses</span>
+                <svg
+                  fill="none"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  width="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                  />
+                </svg>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            </button>
           </Link>
         </div>
       </div>
