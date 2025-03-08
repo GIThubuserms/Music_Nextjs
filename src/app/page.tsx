cb7b501045eaec981c40e4eb2b986bba094a0acf
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-03-08 22:40:21 +0500

    done with ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Herosection from "../components/Herosection";
 import FeaturedCourses from "../components/FeaturedCourses";
+import Stickyxcroll from "@/components/StickyScroll";
+import TestinomialsCard from "@/components/TestinomialsCard";
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
    <Herosection/>
    <FeaturedCourses/>
+   <Stickyxcroll/>
+   <TestinomialsCard/>
     
    </div>
   );
